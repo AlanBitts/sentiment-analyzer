9ef05a20df6afaee5d2ec0656dce11054385f383
AlanBitts <rui-viveiros@hotmail.com>
2024-07-03 11:11:47 +0100

    Initial commit

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+
+app = Flask("Sentiment Analyser")
+
+@app.route("/sentimentAnalyzer")
+def sent_analyzer():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = sentiment_analyzer(text_to_analyze)
+    label = response['label']
+    score = response['score']
+    if label is None:
+        return "Invalid input ! Try again."
+    else:
+        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    ''' This functions executes the flask app and deploys it on localhost:5000
+    '''
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=5000)
